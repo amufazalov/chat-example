2df13aa9826abe29b87d9269bb7b94c166ccb6fe
Airat Mufazalov <airat.mufazalov@yandex.ru>
2018-12-09 21:55:29 +0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var socket = require('socket.io');
+
+//App setup
+var app = express();
+var server  = app.listen(4000, function(){
+    console.log('Listening port 4000')
+});
+ 
+//static files
+app.use(express.static('public'))
+
+//Socket setup
+var io = socket(server);
+
+io.on('connection', function(socket){
+    console.log('made socket connection ', socket.id)
+})
