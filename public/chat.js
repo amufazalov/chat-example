291fabccad3987e96ffa332d294f2d2463561245
Airat Mufazalov <airat.mufazalov@yandex.ru>
2018-12-09 23:08:15 +0300

    add description

--- public/chat.js
+++ public/chat.js
@@ -27,5 +27,5 @@ socket.on('chat', function(data){
 })
 
 socket.on('typing', function(data){
-    feedback.innerHTML = '<p><em>' + data + ' is typing a message... </em></p>'
+    feedback.innerHTML = '<p><em>' + data + ' пишет сообщение... </em></p>'
 })
